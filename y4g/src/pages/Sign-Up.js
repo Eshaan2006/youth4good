4a91cca921d9d0c85922f72ea90a660d477a9bea
eshetty <99435979+Eshaan2006@users.noreply.github.com>
2024-09-07 12:06:27 -0700

    Update Sign-Up.js

--- y4g/src/pages/Sign-Up.js
+++ y4g/src/pages/Sign-Up.js
@@ -1,7 +1,7 @@
 // src/pages/SignUp.js
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { FIREBASE_APP, FIREBASE_AUTH } from './FirebaseConfig';
+import { FIREBASE_APP, FIREBASE_AUTH } from './data/FirebaseConfig';
 import { TextField, Button, Container, Typography, Box, Alert, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { setDoc, doc } from 'firebase/firestore';
 import { FIRESTORE_DB } from './FirebaseConfig';
