008206dbb15d6f43f7d329c5b0aafb0b68d05cf6
Aadi Garg <garg.aadi.a@gmail.com>
2024-09-07 12:12:09 -0700

    3

--- y4g/src/pages/Sign-Up.js
+++ y4g/src/pages/Sign-Up.js
@@ -4,7 +4,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { FIREBASE_APP, FIREBASE_AUTH } from './data/FirebaseConfig';
 import { TextField, Button, Container, Typography, Box, Alert, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { setDoc, doc } from 'firebase/firestore';
-import { FIRESTORE_DB } from './FirebaseConfig';
+import { FIRESTORE_DB } from './data/FirebaseConfig';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
